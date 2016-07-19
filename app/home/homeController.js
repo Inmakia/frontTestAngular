47436910bb29c1694a0aee3c902fdbd161297920
Sonia Martínez Martín <smartinez.martin@atsistemas.com>
2016-07-19 15:34:11 +0200

    Sorting filter

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -4,5 +4,9 @@ module.exports = function ($scope, $rootScope, shipmentsService, $translate) {
   shipmentsService.getShipments().success(function(data){
     $scope.shipments = data.shipments;
   });
-  $scope.$watch($scope.sortType, $scope.sortReverse);
+
+  $scope.sortBy = function(sortType) {
+    $scope.sortReverse = ($scope.sortType === sortType) ? !$scope.sortReverse : false;
+    $scope.sortType = sortType;
+  };
 };
