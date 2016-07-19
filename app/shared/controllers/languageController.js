5adacd04129e96835f268369739c67489f55f009
Sonia Martínez Martín <smartinez.martin@atsistemas.com>
2016-07-19 16:11:15 +0200

    Removing console.log

--- app/shared/controllers/languageController.js
+++ app/shared/controllers/languageController.js
@@ -1,6 +1,5 @@
 module.exports = function($scope, $rootScope, $route){
     $scope.applyLanguage = function(lang){
-      console.log(lang);
         $rootScope.appLanguage = lang;
         $route.reload();
     }
